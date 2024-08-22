7307a75f3215001426994954a09a2a121d0ddb97
nbaertsch <nbaertsch@github.com>
2024-08-21 21:39:41 -0400

    some stuff has changed

--- src/PoolProxy/PoolProxy.nim
+++ src/PoolProxy/PoolProxy.nim
@@ -9,6 +9,16 @@ template debug(a: untyped) =
         a
 
 
+#var ppCS: CRITICAL_SECTION # Critical Section for poolproxy calls
+
+# initialize critical section on import
+# ! OPSEC !
+# https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-initializecriticalsectio
+#if 0 == InitializeCriticalSectionEx(ppCS.addr, 100, CRITICAL_SECTION_NO_DEBUG_INFO):
+#    debug:
+#        echo "Failed to initialize poolproxy critical section"
+
+
 dinvokeDefine(
     TpAllocWork,
     "ntdll.dll",
@@ -42,132 +52,79 @@ dinvokeDefine(
 
 {.passL: "-Wl,--image-base -Wl,0x10000000".}
 proc workCallbackStub*(instance: PTP_CALLBACK_INSTANCE, ctx: PVOID, work: PTP_WORK) {.stdcall, asmNoStackFrame.} =
+    #EnterCriticalSection(ppCS.addr)
+    #LeaveCriticalSEctionWhenCallbackReturns(instance, ppCS.addr)
+
     asm """
     code:
+        push rbx                # this is popped in our gadget
         mov rbx, rdx
-        mov r11, [rbx]
+        mov r11, [rbx]          # sentinel 1
 
-        mov rax, [rbx + 0x8]    # no cmp since we jump to rax
+        mov rax, [rbx + 0x8]    # proxied funcAddr
 
-        cmp r11, [rbx + 0x10]
+        sub rsp, 0x40           # add stack space to account for our gadget
+
+        cmp r11, [rbx + 0x18]   # reg arg 1
         je morecode
-        mov rcx, [rbx + 0x10]
+        mov rcx, [rbx + 0x18]
 
-        cmp r11, [rbx + 0x18]
+        cmp r11, [rbx + 0x20]   # reg arg 2
         je morecode
-        mov rdx, [rbx + 0x18]
+        mov rdx, [rbx + 0x20]
 
-        cmp r11, [rbx + 0x20]
+        cmp r11, [rbx + 0x28]   # reg arg 3
         je morecode
-        mov r8, [rbx + 0x20]
+        mov r8, [rbx + 0x28]
 
-        cmp r11, [rbx + 0x28]
+        cmp r11, [rbx + 0x30]   # reg arg 4
         je morecode 
-        mov r9, [rbx + 0x28]
+        mov r9, [rbx + 0x30]
 
-        cmp r11, [rbx + 0x30]
-        je morecode
-        mov r10, [rbx + 0x30] 
-        mov [rsp+0x20], r10
-
-        cmp r11, [rbx + 0x38]
+        cmp r11, [rbx + 0x38]   # stack arg 1
         je morecode
         mov r10, [rbx + 0x38] 
-        mov [rsp+0x28], r10
+        mov [rsp+0x20], r10
 
-        cmp r11, [rbx + 0x40]
+        cmp r11, [rbx + 0x40]   # stack arg 2
         je morecode
         mov r10, [rbx + 0x40] 
-        mov [rsp+0x30], r10
+        mov [rsp+0x28], r10
 
-        cmp r11, [rbx + 0x48]
+        cmp r11, [rbx + 0x48]   # stack arg 3
         je morecode
         mov r10, [rbx + 0x48] 
+        mov [rsp+0x30], r10
+
+        cmp r11, [rbx + 0x50]   # stack arg 4
+        je morecode
+        mov r10, [rbx + 0x50] 
         mov [rsp+0x38], r10
 
-        # There is no more space available in the stack frame for the work callback from `TpAllocPool`
-        # actually there is exactly one more qword available now that we are fucking with the stack frame and overwriting homing space
-
+        # There is no more space available in the forged stack frame, at least for this particular gadget.
+    
     morecode:
-        lea r11, evenmorecode
-        push r11
+        # we are reting to: (48 89 03 48 83 c4 40 5b c3) in ntdll.dll,RtlPcToFileHeader
+            # mov [rbx], rax
+            # add rsp, 40
+            # pop rbx
+        
+        push [rbx + 0x10]       # address of our gadget
         jmp rax
 
-    evenmorecode:
-        mov [rbx], rax
     """
 
-
-proc buildContextTpWork*(funcAddr: FARPROC, args: openArray[SIZE_T]): seq[SIZE_T] =
-    ## Builds the context struct used for TP_WORK_CALLBACK's
-    var context: seq[SIZE_T]
-    let high = args.high
-    
-    var
-        rand = urandom(sizeof(SIZE_T))
-        sentinel = cast[ptr SIZE_T](rand[0].addr)[]
-        sentinelIsSafe: bool = true
-    
-    # check sentinel safety
-    if (sentinel == cast[SIZE_T](funcAddr)): sentinelIsSafe = false
-    for i in 0..high:
-        if sentinel == args[i]: sentinelIsSafe = false
-
-    # loop to ensure a safe sentinel
-    while not sentinelIsSafe:
-        rand = urandom(sizeof(SIZE_T))
-        sentinel = cast[ptr SIZE_T](rand[0].addr)[]
-        sentinelIsSafe = true
-        # check sentinel safety
-        if (sentinel == cast[SIZE_T](funcAddr)): sentinelIsSafe = false
-        for i in 0..high:
-            if sentinel == args[i]: sentinelIsSafe = false
-    
-    echo sentinel.toHex() # debug
-    context.add(sentinel)
-    context.add(cast[SIZE_T](funcAddr))
-    
-    for i in 0..high:
-        if i <= 3:
-            context.add(args[i]) # reg args
-        else:
-            context.add(args[high-(i-4)]) # stack args, inverted order
-    
-    context.add(sentinel)
-    
-    return context
+proc getRtlPcToFileHeaderGadget(): SIZE_T =
+    let funcAddr = cast[SIZE_T](hashAsciiStatic("ntdll.dll").getProcAddressByHash(hashAsciiStatic("RtlPcToFileHeader")))
+    return funcAddr + 36 # trust me bro, or look for the bytes: 48 89 03 48 83 c4 40 5b c3
 
 
 proc coerceToSizeT*[T](t: T): SIZE_T =
     return cast[SIZE_T](t)
 
-#[
-macro poolproxy*(funcName: untyped, libName: untyped, args: varargs[SIZE_T, coerceToSizeT]): SIZE_T =
-    let
-        funcNameStr = funcName.strVal
-        libNameStr = libName.strVal
-
-    quote do:
-        #if `args`.len > 8: {.fatal:"poolproxy max args exceeded (8)".}
-        let funcAddr: FARPROC = (hashAsciiStatic(`libNameStr`).getProcAddressByHash(hashAsciiStatic(`funcNameStr`)))
-        var castedArgs: seq[SIZE_T]
-        var context = buildContextTpWork(funcAddr, `args`)
-
-        # create the work object and execute it.
-        var
-            pWork: PTP_WORK = NULL
-            status = TpAllocWork(&pWork, workCallbackStub.PTP_WORK_CALLBACK, context[0].addr, NULL)  
-        if status != 0:
-            dbgEcho "TpAllocWork failed: ntstatus= ", toHex(status)
-        TpPostWork(pWork)
-        TpWaitForWork(pWork, FALSE)
-        TpReleaseWork(pWork)
-        context[0]
-        
-]#
 
 macro poolproxy*(funcName: untyped, libName: untyped, args: varargs[SIZE_T, coerceToSizeT]): untyped =
-    #if args.len > 8: {.fatal:"poolproxy max args exceeded (8)".}
+    ## Max of 8 args, anything more will fail
     let
         funcNameStr = funcName.strVal
         libNameStr = libName.strVal
@@ -200,6 +157,7 @@ macro poolproxy*(funcName: untyped, libName: untyped, args: varargs[SIZE_T, coer
         # add sentinel and funcAddr to the context struct
         context.add(sentinel)
         context.add(cast[SIZE_T](funcAddr))
+        context.add(getRtlPcToFileHeaderGadget())
 
         # add the rest of the args to the context struct, inverting the stack args order
         for i in 0..high:
@@ -226,4 +184,5 @@ macro poolproxy*(funcName: untyped, libName: untyped, args: varargs[SIZE_T, coer
         TpPostWork(pWork)
         TpWaitForWork(pWork, FALSE)
         TpReleaseWork(pWork)
-        context[0]
+
+        context[0] # return the rop-captured return value of the proxied function
